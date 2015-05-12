9a586f1a50db599c31df11f4027d7fdd3c39dd37
Fabio Rehm <fgrehm@gmail.com>
2015-05-12 18:46:06 -0300

    Alerta indicando que não existem prescricoes em aberto para o paciente

--- resources/views/atendimentos/show.blade.php
+++ resources/views/atendimentos/show.blade.php
@@ -5,6 +5,9 @@
 		<h1>{{ $atendimento->paciente->nome }} <span class="badge">{{ $atendimento->unidade->nome }}</span></h1>
 	</div>
 	<div class="alert alert-info">
+		<p>Este paciente não possui nenhuma prescrição cadastrada</p>
+	</div>
+	<div class="alert alert-info">
 		<p>Aqui vão as informações do paciente</p>
 	</div>
 @stop
