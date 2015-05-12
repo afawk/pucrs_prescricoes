8eaec93c41ab079aabf3c3820d17f8b3ec67f4ac
Anderson Fraga <contato@andersonfraga.net>
2015-05-12 16:45:50 +0000

    Corrigido nome ambíguo de classe

--- database/migrations/2015_05_12_163547_add_fields_atendimentos_table.php
+++ database/migrations/2015_05_12_163547_add_fields_atendimentos_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAtendimentosTable extends Migration {
+class AddFieldsAtendimentoTable extends Migration {
 
 	/**
 	 * Run the migrations.
