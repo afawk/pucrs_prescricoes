a42ca6a42608f9829b8fdec01d59ea6da26f60fa
Fabio Rehm <fgrehm@gmail.com>
2015-05-12 18:31:40 -0300

    Corrigido nome da classe de migration

--- database/migrations/2015_05_12_163547_add_fields_atendimentos_table.php
+++ database/migrations/2015_05_12_163547_add_fields_atendimentos_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFieldsAtendimentoTable extends Migration {
+class AddFieldsAtendimentosTable extends Migration {
 
 	/**
 	 * Run the migrations.
