f10cd18e65719e637ffdecd51148f0e3a79f31f7
Anderson Fraga <contato@andersonfraga.net>
2015-06-28 21:49:32 +0000

    Controller de busca de dados

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,30 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Business\SearchItens;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+	public function index(Request $request)
+	{
+		if (!$request->isXMLHttpRequest()) {
+			return false;
+		}
+
+		if (!$request->input('item') || !$request->input('search')) {
+			return response()->json(array('fail' => 'need item and search elements'), 400);
+		}
+
+		$busca = [
+		    $request->input('item') => $request->input('search'),
+		];
+
+		$search = new SearchItens();
+		$result = $search->by($busca);
+
+		return response()->json($result);
+	}
+};
