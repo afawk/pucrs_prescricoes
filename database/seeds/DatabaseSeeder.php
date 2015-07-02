b59629bf83d8d52f9ed156e164784d56525b7a89
Fabio Rehm <fgrehm@gmail.com>
2015-07-02 13:15:51 -0300

    Desabilita seed de sessoes para manter os dados que já estão em producao agora

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder {
 
 		$this->seedUnidades();
 		$this->seedMedicamentos();
-        $this->seedSessions();
+        // $this->seedSessions();
 	}
 
 	private function seedUnidades()
